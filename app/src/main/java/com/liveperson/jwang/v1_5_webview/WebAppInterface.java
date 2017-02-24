0f542aeb3739e58ab61efd783d75610bdee84a8c
Morgan Lemke <morganl2010@gmail.com>
2017-02-24 15:27:24 -0500

    All variables can now be changed
    added phone number

--- app/src/main/java/com/liveperson/jwang/v1_5_webview/WebAppInterface.java
+++ app/src/main/java/com/liveperson/jwang/v1_5_webview/WebAppInterface.java
@@ -11,6 +11,9 @@ import com.liveperson.messaging.sdk.api.LivePerson;
 
 /**
  * Created by mlemke on 2/9/17.
+ * Last edited 2/24/2017
+ * currently all functionality is working
+ * please reach out to mlemke@liveperson.com for questions
  */
 
 public class WebAppInterface extends MainActivity{
@@ -18,6 +21,9 @@ public class WebAppInterface extends MainActivity{
     Activity A;
     String appID;
     String BrandID;
+    String phone;
+    String nameFirst;
+    String nameLast;
 
 
     /** Instantiate the interface and set the context */
@@ -25,13 +31,26 @@ public class WebAppInterface extends MainActivity{
         mContext = c;
     }
 
+    /** All functions bellow designed to be called from the within
+     * the app
+     **/
     //initialize the Activity (pass by reference)
     public void setActivity(Activity mainA){
         A=mainA;
     }
+    //set initial phone
+    public void setInternalPhone(String phoneIn){
+        phone=phoneIn;
+    }
+    //lets you set base values
+    public void setAppValues(String appIDIn, String BrandIDIn) {
+        appID=appIDIn;
+        BrandID=BrandIDIn;
+    }
 
-    /** Show a toast from the web page */
-    //@JavascriptInterface
+    /** All functions bellow designed to be called from the webpage
+     * be careful of inherient security risks
+     **/
 
     //basically on click function
     @JavascriptInterface
@@ -42,19 +61,25 @@ public class WebAppInterface extends MainActivity{
     //allows you to change name but everything must be repassed
     @JavascriptInterface
     public void setName(String nameFirstIn, String nameLastIn) {
-        Toast.makeText(mContext, "Change Name", Toast.LENGTH_SHORT).show();
-        LivePerson.setUserProfile(appID,nameFirstIn,nameLastIn,"858-926-8138");
+        nameFirst=nameFirstIn;
+        nameLast=nameLastIn;
+        LivePerson.setUserProfile(appID,nameFirstIn,nameLastIn, phone);
+    }
+    //allows you to change phone but everything must be repassed
+    @JavascriptInterface
+    public void setPhone(String phoneIn) {
+        phone=phoneIn;
+        LivePerson.setUserProfile(appID,nameFirst,nameLast, phoneIn);
     }
     //just test function to call
     @JavascriptInterface
     public void toast(){
-        //Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
-        System.out.println("test");
+        Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
     }
-    //lets you set base values
-    public void setAppValues(String appIDIn, String BrandIDIn) {
-        appID=appIDIn;
-        BrandID=BrandIDIn;
+
     //lets you display to the user something has been done by showing a toast
+    @JavascriptInterface
+    public void tellUser(String toast){
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
     }
 }
